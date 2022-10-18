08f33a80fd3ba172c21c4b6d39757255eb47ae91
charles.gallagher07 <charles.gallagher07@gmail.com>
2022-10-17 20:24:11 -0500

    convert exportExcel.py to class exportExcel

--- exportExcel.py
+++ exportExcel.py
@@ -1,36 +1,51 @@
 import os
-from dotenv import load_dotenv
 from DBConnect import mysql_connection
 from openpyxl import Workbook
 
 
-load_dotenv()
-config = {
-'user': os.getenv('USERNAME'),
-'password': os.getenv('PASSWORD'),
-'host': os.getenv('HOST'),
-'database': os.getenv('DATABASE')
- }
+class exportExcel:
+    def __init__(self, config) -> None:
+        self.config = {
+    'user': os.getenv('USERNAME'),
+    'password': os.getenv('PASSWORD'),
+    'host': os.getenv('HOST'),
+    'database': os.getenv('DATABASE')
+    }
+
+    def exportToExcelWorkbook(self, filepath = "."):
+            
+        with mysql_connection(**config) as conn:
+            with conn.cursor() as cursor:
+
+                cursor.execute("SHOW TABLES;")
+                tables = {i[0]:[] for i in cursor }
+
+                for table in tables:
+                    cursor.execute(f"SHOW columns FROM {table}")
+                    tables[table] = [column[0] for column in cursor.fetchall()]
+
+        # create excel workbook
+        wb = Workbook()
+
+        for table  in tables.keys():
+            _sheet = wb.create_sheet(title=table)
+            _sheet.append(tables[table])
 
 
-with mysql_connection(**config) as conn:
-    with conn.cursor() as cursor:
-
-        cursor.execute("SHOW TABLES;")
-        tables = {i[0]:[] for i in cursor }
-
-        for table in tables:
-            cursor.execute(f"SHOW columns FROM {table}")
-            tables[table] = [column[0] for column in cursor.fetchall()]
+        del wb['Sheet']
+        print(config['database'])
+        wb.save(filename=f"{filepath}/{config['database']}.xlsx")
 
 
-# create excel workbook
-wb = Workbook()
+if __name__ == "__main__":
 
-for table  in tables.keys():
-    _sheet = wb.create_sheet(title=table)
-    _sheet.append(tables[table])
-
-
-del wb['Sheet']
-wb.save(filename="test.xlsx")
+    from dotenv import load_dotenv
+    load_dotenv()
+    config = {
+        'user': os.getenv('USERNAME'),
+        'password': os.getenv('PASSWORD'),
+        'host': os.getenv('HOST'),
+        'database': os.getenv('DATABASE')
+        }
+    db = exportExcel(config)
+    db.exportToExcelWorkbook()
