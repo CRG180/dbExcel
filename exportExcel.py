74b03afa1f9110ca72f077ddff4c3fb9f03f6c69
charles.gallagher07 <charles.gallagher07@gmail.com>
2022-10-17 21:20:40 -0500

    add check creds, if evn does not have creds then exportExcel ask for user input, add place holders for future functionality.

--- exportExcel.py
+++ exportExcel.py
@@ -4,19 +4,33 @@ from openpyxl import Workbook
 
 
 class exportExcel:
-    def __init__(self, config) -> None:
+    """Add doc string"""
+    def __init__(self) -> None:
         self.config = {
     'user': os.getenv('USERNAME'),
     'password': os.getenv('PASSWORD'),
     'host': os.getenv('HOST'),
     'database': os.getenv('DATABASE')
     }
+        self._check_config()
+        self._check_connection()
 
-    def exportToExcelWorkbook(self, filepath = "."):
+    def _check_config(self)-> None:
+        """ function checks to see if credentials are missing 
+            or not present in env variables"""
+        for key in self.config.keys():
+            if self.config[key] == None:
+                self.config[key] = input(f"Enter Database {key}: ")
+    
+    def _check_connection(self) -> None:
+        " function checks to see if connection is viable with provided credintials"
+        pass
+
+    def exportMysqlToExcelWorkbook(self, filepath = ".") -> None:
+        "Add Doc String"
             
-        with mysql_connection(**config) as conn:
+        with mysql_connection(**self.config) as conn:
             with conn.cursor() as cursor:
-
                 cursor.execute("SHOW TABLES;")
                 tables = {i[0]:[] for i in cursor }
 
@@ -27,25 +41,27 @@ class exportExcel:
         # create excel workbook
         wb = Workbook()
 
-        for table  in tables.keys():
+        for table in tables.keys():
             _sheet = wb.create_sheet(title=table)
             _sheet.append(tables[table])
 
-
         del wb['Sheet']
-        print(config['database'])
-        wb.save(filename=f"{filepath}/{config['database']}.xlsx")
+        file = f"{filepath}/{self.config['database']}.xlsx"
+        wb.save(filename=file)
+        print(f"Database has been exported to {file}")
+
+    def exportSqlservertoExcelWorkbook(self) -> None:
+        "Add Docstring"
+        pass
+
+    def exportSqliteToExcelWorkbook(self) -> None:
+        "Add Docstring"
+        pass
 
 
 if __name__ == "__main__":
 
     from dotenv import load_dotenv
     load_dotenv()
-    config = {
-        'user': os.getenv('USERNAME'),
-        'password': os.getenv('PASSWORD'),
-        'host': os.getenv('HOST'),
-        'database': os.getenv('DATABASE')
-        }
-    db = exportExcel(config)
-    db.exportToExcelWorkbook()
+    db = exportExcel()
+    db.exportMysqlToExcelWorkbook()
